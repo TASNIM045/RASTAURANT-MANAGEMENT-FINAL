21e1a431296b8325fa70bca79dea40afd338e774
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-05-16 14:12:57 +0600

    user class

--- restaurant.py
+++ restaurant.py
@@ -1,7 +1,7 @@
 from abc import ABC
 
 class User(ABC):
-    def __init__(self,name,email,phone):
+    def __init__(self,name,email,address):
         self.name = name
         self.email = email
-        self.phone = phone
+        self.address = address
