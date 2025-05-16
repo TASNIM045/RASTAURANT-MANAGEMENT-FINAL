69528f9a47720121fda44a7303a373bd07b74a2a
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-05-16 14:19:58 +0600

    initial customer and adimn class

--- restaurant.py
+++ restaurant.py
@@ -5,3 +5,27 @@ class User(ABC):
         self.name = name
         self.email = email
         self.address = address
+
+class Customer(User):
+    def __init__(self, name, email, address):
+        super().__init__(name, email, address)
+
+
+class Adimin(User):
+    def __init__(self, name, email, address):
+        super().__init__(name, email, address)
+        self.password = "1221"
+
+    def add_item(self,item):
+        pass
+    def remove_item(self,item):
+        pass
+    def add_customar(self,customer):
+        pass
+    def remove_customer(self,customer):
+        pass
+
+
+class Restaurent:
+    def __init__(self,name):
+        self.name = name
